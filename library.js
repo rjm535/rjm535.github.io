3a3a72893f4aa65e02129e52ceb8bb92cef3e245
Ryan Martin <rjm535@gmail.com>
2020-01-17 19:10:01 -0600

    adding a deleted section

--- library.js
+++ library.js
@@ -26,7 +26,6 @@ function msToTime(s) {
 
 function myTimer() {
     var d = new Date();
-    // document.getElementById("demo").innerHTML = d.toLocaleTimeString();
     if(current_item) {
         document.getElementById("current_item").innerHTML = todo_list[current_item]["name"] 
                                                             + " --  "
@@ -36,7 +35,8 @@ function myTimer() {
 var myTimer = setInterval(myTimer, 1000);
 
 var todo_list = window.localStorage.todo_list ? JSON.parse(window.localStorage.todo_list) : [];
-displayList(todo_list);
+var deleted_list = window.localStorage.deleted_list ? JSON.parse(window.localStorage.deleted_list) : [];
+displayLists();
 var current_item;
 var start_time;
 
@@ -45,24 +45,37 @@ function saveField() {
     if(value.value != "") {
         todo_list.push({"name" : value.value, "time" : 0})
         window.localStorage.todo_list = JSON.stringify(todo_list)
-        displayList(todo_list);
+        //displayList(todo_list);
+        displayLists();
         value.value = "";
     }
 }
 
-function deleteItem(target) {
+function softDeleteItem(target){
     if(target.parentElement.id == current_item) {
         current_item = null;
         document.getElementById("current_item").innerHTML = "Current Item...";
         start_time = null;
     }
+    deleted_list.push(todo_list[target.parentElement.id])
     if(todo_list.length > 1) {
         todo_list.splice(target.parentElement.id, 1);
     } else {
         todo_list = [];
     }
     window.localStorage.todo_list = JSON.stringify(todo_list);
-    displayList(todo_list);
+    window.localStorage.deleted_list = JSON.stringify(deleted_list);
+    displayLists();
+}
+
+function hardDeleteItem(target){
+    if(deleted_list.length > 1) {
+        deleted_list.splice(target.parentElement.id, 1);
+    } else {
+        deleted_list = [];
+    }
+    window.localStorage.deleted_list = JSON.stringify(deleted_list);
+    displayLists();
 }
 
 function startItem(target) {
@@ -78,23 +91,23 @@ function startItem(target) {
                                                         + "  --  "
                                                         + msToTime(0);
     window.localStorage.todo_list = JSON.stringify(todo_list);
-    displayList(todo_list); 
+    displayLists(todo_list); 
 }
 
-function displayList(list) {
+function displayLists() {
     temp_div = '<ul style="list-style-type:none">'
     for(i in todo_list) {
         if(i == current_item) {
             temp_div += '<li id="' + i
                      + '">'
-                     + '<button onclick="deleteItem(this)">x</button>'
+                     + '<button onclick="softDeleteItem(this)">x</button>'
                      + todo_list[i]["name"] + " --  " + msToTime(todo_list[i]["time"])
                      + '</li>';
         }
         else {
             temp_div += '<li id="' + i
                      + '">'
-                     + '<button onclick="deleteItem(this)">x</button>'
+                     + '<button onclick="softDeleteItem(this)">x</button>'
                      + todo_list[i]["name"] + " -- " +  msToTime(todo_list[i]["time"])
                      + '<button onclick="startItem(this)">Start</button>'
                      + '</li>';
@@ -102,4 +115,14 @@ function displayList(list) {
     }
     temp_div += '</ul>'
     document.getElementById('todo_list').innerHTML = temp_div
+    temp_div = '<ul style="list-style-type:none">'
+    for(i in deleted_list) {
+        temp_div += '<li id="' + i
+                 + '">'
+                 + '<button onclick="hardDeleteItem(this)">x</button>'
+                 + deleted_list[i]["name"] + " -- " +  msToTime(deleted_list[i]["time"])
+                 + '</li>';
+    }
+    temp_div += '</ul>'
+    document.getElementById('completed_list').innerHTML = temp_div
 }
